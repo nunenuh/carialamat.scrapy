38981724c43ece603965e6b681490d274076195f
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2020-11-05 10:02:16 +0800

    feat: add converter from json to csv that compatible to pandas

---
+++ converter.py
@@ -0,0 +1,38 @@
+import json
+import pandas as pd
+import fire
+from tqdm import tqdm
+
+def scrapy2dframe(json_file):    
+    with open(json_file) as file:
+        datas = json.load(file)
+    
+    alamat = {
+        'name':[],
+        'address': [],
+        'region':[]
+    }
+
+    for data in tqdm(datas):
+        alamat['name'].append(data['name'].strip())
+        alamat['address'].append(data['address'].strip())
+        alamat['region'].append(data['region'])
+
+    df = pd.DataFrame(alamat)
+    
+    return df
+
+
+def json2csv(src_path, dst_path):
+    """[summary]
+
+    Args:
+        src_path ([type]): [description]
+        dst_path ([type]): [description]
+    """
+    dframe:pd.DataFrame = scrapy2dframe(src_path)
+    dframe.to_csv(dst_path, index=False, index_label=False)
+    
+
+if __name__ == "__main__":
+    fire.Fire(json2csv)
